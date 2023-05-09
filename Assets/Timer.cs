92565369b296d506a546a556dc18166afdac7d65
c3kimball <c3kimball@student.bridgew.edu>
2023-05-09 14:28:07 -0400

    For real this time, pushing to main

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text timerText;
+    private float startTime;
+    private bool fin;
+    public GameObject player;
+
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        
+        if (player.transform.position.x < 40)
+        {
+            Debug.Log("Finished");
+            fin = true;
+        }
+        if (fin)
+        {
+            finish();
+            return;
+        }
+        float currentTime = Time.time - startTime;
+
+        string minutes = ((int)currentTime / 60).ToString();
+        string seconds = (currentTime % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+
+    }
+
+    public void finish()
+    {
+        timerText.color = Color.yellow;
     }
 }
